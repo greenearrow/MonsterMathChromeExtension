192f7459f6cc42a5831c48aa9559dc1128503f4d
Ben Greene <greenelantern@gmail.com>
2022-01-27 08:07:35 -0500

    super basic load options that does not work

--- options.js
+++ options.js
@@ -1,3 +1,4 @@
+
 function save_options() {
     var num_chars = document.getElementById('num_chars').value
     var avg_level = document.getElementById('avg_level').value
@@ -5,6 +6,12 @@ function save_options() {
     chrome.storage.local.set({num_chars: num_chars});
     chrome.storage.local.set({avg_level: avg_level});
     chrome.storage.local.set({difficulty: difficulty});
+}
+
+function load_options() {
+    chrome.storage.local.get(num_chars, function(){
+        document.getElementById('num_chars').value = key
+    });
 }
 const btn = document.getElementById('btn_set')
 btn.addEventListener("click", function () {
